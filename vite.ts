5f3fdb0d4d72be3472ccf9dbb43eefe6715d046f
HACKS234 <100372203+HACKS234@users.noreply.github.com>
2025-02-17 09:04:36 -0600

    Update vite.ts

--- vite.ts
+++ vite.ts
@@ -52,10 +52,10 @@ export async function setupVite(app: Express, server: Server) {
         __dirname,
         "..",
         "client",
-        "index.html",
+        "clientindex.html",
       );
 
-      // always reload the index.html file from disk incase it changes
+      // always reload the clientindex.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
       template = template.replace(
         `src="/src/main.tsx"`,
@@ -81,8 +81,8 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
+  // fall through to clientindex.html if the file doesn't exist
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(path.resolve(distPath, "clientindex.html"));
   });
 }
